91a10392ca7c8a6ac893404f1abbf10571da3ef8
Alacy Serrão <andersonalx.sa@gmail.com>
2025-07-14 15:59:24 -0400

    Adicionado botões de desconto e adicionar produto

--- src/Produto.js
+++ src/Produto.js
@@ -1,21 +1,40 @@
 import { useState } from 'react';
 
 function Produto(props) {
-  const [preco, setPreco] = useState(props.preco);
+  const preco_ant = props.preco;
 
+  const [preco, setPreco] = useState(props.preco);
+  const [quantidade, setQuantidade] = useState(0);
+ 
   const aplicarDesconto = () => {
     const desconto = preco * 0.1; // 10%
     setPreco((preco - desconto).toFixed(2));
   };
+  const Restaura_preco = () => {
+   
+    setPreco((preco_ant).toFixed(2));
+  };
+
+  const adicionarAoCarrinho = () => {
+    setQuantidade(quantidade + 1);
+  }
+  const total = (quantidade * preco).toFixed(2);
     return <div style={{ 
       border: "1px solid #ccc", 
       padding: "12px", 
       margin: "10px 0", 
       borderRadius: "6px", 
-      backgroundColor: "#f4f4f4" 
+      backgroundColor: "#f4f4f4" ,
+      width: "260px"
     }}>
-    <><p>Produto : {props.nome}!</p><p>Preco R$ {preco}</p></>
-    <button onClick={aplicarDesconto}>Aplicar 10% de desconto</button>
+    <h3>Produto : {props.nome}!</h3>
+    <p>Preco R$ {preco}</p>
+    <p>Quantidade : {quantidade}</p>
+    <p><strong> Total : R$ {total}</strong></p>
+
+    <button onClick={aplicarDesconto} style={{marginRight:"8px"}}>Aplicar 10% de desconto</button>
+    <button onClick={Restaura_preco} style={{marginRight:"8px"}}>Restaura Preço</button>
+    <button onClick={adicionarAoCarrinho}>Adicionar ao Carrinho</button>
     </div>
     
     }
